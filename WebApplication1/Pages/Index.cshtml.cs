3ad95c1a47ae7ac03c32ef4a8cd26c7f37f3063c
Ilya Galuzinskii <IlyaGall01@gmail.com>
2025-05-28 16:18:29 +0300

    ajax

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -5,16 +5,28 @@ namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            Message = "Время:";
         }
+
+        public string PrintTime() => DateTime.Now.ToLongTimeString();
 
         public void OnGet()
         {
 
+        }
+
+
+        // Новый метод для AJAX-запроса
+        public IActionResult OnGetCurrentTime()
+        {
+            return new JsonResult(new { time = DateTime.Now.ToLongTimeString() });
         }
     }
 }
