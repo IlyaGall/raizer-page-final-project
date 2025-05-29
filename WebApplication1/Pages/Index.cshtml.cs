bfa974e972112d03216d7955e9950e00896f8bb7
Ilya Galuzinskii <IlyaGall01@gmail.com>
2025-05-29 07:06:54 +0300

    +

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -28,5 +28,29 @@ namespace WebApplication1.Pages
         {
             return new JsonResult(new { time = DateTime.Now.ToLongTimeString() });
         }
+
+        public JsonResult OnGetSearch(string query)
+        {
+            // Здесь реализуйте логику поиска товаров
+            // Пример:
+            var searchResults = new List<Product>
+        {
+            new Product { Id = 1, Name = query + " 1", Description = "Описание " + query + " 1", Price = 100 },
+            new Product { Id = 2, Name = query + " 2", Description = "Описание " + query + " 2", Price = 200 },
+            new Product { Id = 3, Name = query + " 3", Description = "Описание " + query + " 3", Price = 300 }
+        };
+
+            return new JsonResult(searchResults);
+        }
+
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+        }
+
+
     }
 }
